49d206f3e06ab4c931b2b3b8ae532393f2d920aa
Andres Hernandez <107950480+REFH4CK@users.noreply.github.com>
2024-09-16 13:47:35 -0400

    Update AboutMe.jsx

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -10,7 +10,7 @@ function AboutMe() {
         <article className="container-aboutMe">
           <p className="container-aboutMe-info">
             Hi there👋🏻! My name is <span className="aboutMe-name"><strong>Andrés Hernández</strong>.</span><br/><br/>
-            I have 21 years old and passionate about technology, computers and
+            I'm 21 years old and passionate about technology, computers and
             programming. I like to offer technological solutions to solve
             problems and I enjoy web development as much as a good cup of
             coffee.
