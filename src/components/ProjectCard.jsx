c6d29346e52e5fde3236acce1e5a8fe54238fbbe
REFH4CK <andres29ehr@gmail.com>
2024-09-17 15:44:42 -0400

    ⚙️ Import pixels mission image and fix url

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,10 +2,12 @@ import "./ProjectCard.css";
 import projects from "../json/projects.json";
 import tracker from "../assets/tracker.png";
 import bitmates from "../assets/bitmates-info.png";
+import pixels_missions from "../assets/pixels-missions.png";
 
 const images = {
   bitmates,
   tracker,
+  pixels_missions
 };
 
 function ProjectCard() {
@@ -15,7 +17,7 @@ function ProjectCard() {
         {projects.map((project, index) => (
           <article key={index} className="projectCard">
             <div className="projectPreview">
-              <a href={project.url}>
+              <a href={project.url} target="_blank">
                 <img
                   src={images[project.presentation_image]}
                   alt={project.name}
