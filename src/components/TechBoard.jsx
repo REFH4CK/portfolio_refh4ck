fadc6dd553a45c6f242fab424d0f5d398c790f93
REFH4CK <andres29ehr@gmail.com>
2024-09-17 15:43:58 -0400

    ⚙️ Add tools, change title and dinamic url on map loop

--- src/components/TechBoard.jsx
+++ src/components/TechBoard.jsx
@@ -1,33 +1,38 @@
-import './TechBoard.css'
+import "./TechBoard.css";
 
 function TechBoard() {
-
   const knowTechs = [
-    {name: "HTML", img: "https://svgl.app/library/html5.svg"},
-    {name: "CSS", img: "https://svgl.app/library/css.svg"},
-    {name: "JavaScript", img: "https://svgl.app/library/javascript.svg"},
-    {name: "React", img: "https://svgl.app/library/react.svg"},
-    {name: "Node.js", img: "https://svgl.app/library/nodejs.svg"},
-    {name: "PHP", img: "https://svgl.app/library/php.svg"},
-    {name: "MySQL", img: "https://svgl.app/library/mysql.svg"},
-    {name: "Git", img: "https://svgl.app/library/git.svg"}
+    { name: "HTML", img: "html5.svg" },
+    { name: "CSS", img: "css.svg" },
+    { name: "JavaScript", img: "javascript.svg" },
+    { name: "React", img: "react.svg" },
+    { name: "Node.js", img: "nodejs.svg" },
+    { name: "PHP", img: "php.svg" },
+    { name: "MySQL", img: "mysql.svg" },
+    { name: "Git", img: "git.svg" },
+    { name: "Vscode", img: "vscode.svg" },
+    { name: "Github", img: "github-dark.svg" },
   ];
 
   return (
     <>
-      <article className='container-board'>
-        <div className='container-board-grid'>
-          <h2 className='container-board-title'>Knowledge Technologies</h2>
+      <article className="container-board">
+        <div className="container-board-grid">
+          <h2 className="container-board-title">Knowledge Technologies and Tools</h2>
           {knowTechs.map((tech, i) => (
-            <div key={i} className='tech'>
-              <img src={tech.img} alt={`Logo ${tech.name}`} className='logo-tech' />
-              <p className='tech-name'>{tech.name}</p>  
+            <div key={i} className="tech">
+              <img
+                src={`https://svgl.app/library/${tech.img}`}
+                alt={`Logo ${tech.name}`}
+                className="logo-tech"
+              />
+              <p className="tech-name">{tech.name}</p>
             </div>
           ))}
         </div>
       </article>
     </>
-  )
+  );
 }
 
-export default TechBoard
+export default TechBoard;
