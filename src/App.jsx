e4741a11e2c24181b39b399857f8fd83116496dc
REFH4CK <refh4ck.dev@gmail.com>
2025-01-14 15:06:25 -0400

    clearing zone

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
-import "./App.css";
-import Header from "./components/Header";
-import NavBar from "./NavBar";
-import AboutMe from "./AboutMe";
-import TechBoard from "./components/TechBoard";
-import ProjectCard from "./components/ProjectCard";
-
-
-
-function App() {
+export function App() {
   return (
     <>
-      <Header />
-      <main className="container">
-        <NavBar />
-        <section className="container-info" id="about">
-          <AboutMe />
-        </section>
-        <section className="container-technologies" id="technologies">
-          <TechBoard />
-        </section>            
-        <section className="container-projects" id="projects">
-          <h2 className='container-projects-title'>Featured projects</h2>
-          <ProjectCard />
-        </section>
+      <main className="">
       </main>
     </>
   );
 }
-
-export default App;
